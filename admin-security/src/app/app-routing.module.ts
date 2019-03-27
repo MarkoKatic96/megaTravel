54053bc8d81c0408c32b483acd1e345d747b037d
Nikola Maric <maricnikola752@gmail.com>
2019-03-27 15:22:14 +0100

    uvedeno rutiranje

--- admin-security/src/app/app-routing.module.ts
+++ admin-security/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { KomunikacijaComponent } from './komunikacija/komunikacija.component';
+import { KreirajSertifikatComponent } from './kreiraj-sertifikat/kreiraj-sertifikat.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PovuciSertifikatComponent } from './povuci-sertifikat/povuci-sertifikat.component';
+import { PostojeciSertifikatiComponent } from './postojeci-sertifikati/postojeci-sertifikati.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'kreirajsert', pathMatch: 'full'},
+  {path: 'kreirajsert', component: KreirajSertifikatComponent},
+  {path: 'komunikacija', component: KomunikacijaComponent},
+  {path: 'povucisert', component: PovuciSertifikatComponent},
+  {path: 'postojecisert', component: PostojeciSertifikatiComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- admin-security/src/app/app.module.ts
+++ admin-security/src/app/app.module.ts
@@ -1,11 +1,23 @@
+import { ListanjeComponent } from './kreiraj-sertifikat/listanje/listanje.component';
+import { KreiranjeComponent } from './kreiraj-sertifikat/kreiranje/kreiranje.component';
+import { KreirajSertifikatComponent } from './kreiraj-sertifikat/kreiraj-sertifikat.component';
+import { KomunikacijaComponent } from './komunikacija/komunikacija.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostojeciSertifikatiComponent } from './postojeci-sertifikati/postojeci-sertifikati.component';
+import { PovuciSertifikatComponent } from './povuci-sertifikat/povuci-sertifikat.component';
 
 @NgModule({
   declarations: [
+    KomunikacijaComponent,
+    KreirajSertifikatComponent,
+    KreiranjeComponent,
+    ListanjeComponent,
+    PostojeciSertifikatiComponent,
+    PovuciSertifikatComponent,
     AppComponent
   ],
   imports: [
